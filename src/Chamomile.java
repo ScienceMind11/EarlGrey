c84c42407564f6f2dff9fcbb492f08dfe35492cd
BuildTools <unconfigured@null.spigotmc.org>
2022-12-06 20:52:52 -0500

    Added playerAttack and monsterAttack functions to Chamomile class

--- src/Chamomile.java
+++ src/Chamomile.java
@@ -4,7 +4,19 @@ class Chamomile {
 
         System.out.println("\nOh no! A " + monster + " has approached!");
         System.out.println("⚔️" + "YOU ARE NOW ENTERING COMBAT" + "⚔️");
-        System.out.println("");
+
+    }
+
+    public static int playerAttack(String weapon, String monster, int monsterHealth, int damage) {
+
+        System.out.println("You strike out at the " + monster + " with your " + weapon + " , dealing " + damage + " damage to it");
+        return monsterHealth - damage;
+
+    }
+
+    public static void monsterAttack(String monster, int playerHealth, int damage) {
+
+        System.out.println("Monster attack");
 
     }
 
